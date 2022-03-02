71c47d00e36e01040122e6fcbda6afeaa5ea8e6a
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-02 15:41:57 +0300

    Added new functionality

--- apptimer.js
+++ apptimer.js
@@ -26,11 +26,14 @@ const discount = document.querySelector('.discount');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
-let futureDate = new Date(2022, 2, 24, 21, 30, 0);
+// let futureDate = new Date(2022, 2, 24, 21, 30, 0);
 // console.log(futureDate);
-
-
+const futureDate = new Date(tempYear,tempMonth,tempDay + 10,11,30,0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours(); 
 const minutes = futureDate.getMinutes();
